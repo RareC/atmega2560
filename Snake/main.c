96435f2684276d5fc35e63f968233c20fea78ced
chrislyford <35743417+chrislyford@users.noreply.github.com>
2018-02-16 16:45:07 +0000

    Turning with tail now works
    Also cleaned up code slightly

--- Snake/main.c
+++ Snake/main.c
@@ -24,17 +24,16 @@ uint8_t has_changed = 0;			//prevents multiple direction changes per step
 
 //Function Prototypes
 void init_buttons();
-void make_snake(uint8_t*,uint8_t*);
+void make_snake(uint8_t*,uint8_t*,uint8_t);
 void move_snake(uint8_t*,uint8_t*,enum direction);
 void store_moves(uint8_t*,uint8_t*,uint8_t);
+void sum_snake();
 
 ISR(INT0_vect)							//when right button pressed
 {
-	if(has_changed == 0)
-	{
+	if(has_changed == 0){				//check if direction has already been modified this step
 		has_changed = 1;
-		switch(dir)
-		{
+		switch(dir){
 			case UP:
 			dir = RIGHT;
 			break;
@@ -57,11 +56,9 @@ ISR(INT0_vect)							//when right button pressed
 
 ISR(INT2_vect)							//when left button pressed
 {
-	if(has_changed == 0)
-	{
+	if(has_changed == 0){				//check if direction has already been modified this step
 		has_changed = 1;
-		switch(dir)
-		{
+		switch(dir){
 			case UP:
 			dir = LEFT;
 			break;
@@ -90,12 +87,10 @@ int main(void)
 	uint8_t *first_row_pos = prev_rows;	//pointer to first element of row history
 	uint8_t *first_col_pos = prev_cols;	//pointer to first element of column history
 	uint8_t grid[8][8];					//positions on matrix
-	uint8_t length = 2;					//length of snake
+	uint8_t length = 8;					//length of snake
 	
 	prev_cols[0] = 0x05;				//starting column
 	prev_rows[0] = 0x02;				//starting row
-	prev_cols[1] = 0x06;
-	prev_rows[1] = 0x02;
 	
 	SPI_data_send(shutdown,0x01);		//disable shutdown mode
 	SPI_clear_all(0x07);				//clear all rows and set scan range to all rows
@@ -104,87 +99,87 @@ int main(void)
     while (1) 
     {
 		has_changed = 0;
-		make_snake(first_col_pos, first_row_pos);
+		make_snake(first_col_pos, first_row_pos, length);
 		store_moves(first_col_pos, first_row_pos,length);
 		move_snake(first_col_pos,first_row_pos,dir);
 
     }
 }
 
-void init_buttons()
-{
+void init_buttons(){
 	//set flags
 	sei();								//enable global interrupt
 	EIMSK |= (1 << INT2) | (1 << INT0); //enable interrupt when PD0 or PD2 go low
 	EICRA |= (1<< ISC01) | (1<< ISC21);//enable interrupt on falling edge									
 }
 
-void make_snake(uint8_t *col,uint8_t *row)
-{
+void make_snake(uint8_t *col,uint8_t *row, uint8_t len){
+		
 	SPI_clear_all(0x07);
-	SPI_data_send(*col, *row);
-	SPI_data_send(*(col+1),*(row+1));
-	_delay_ms(1000);
+	//iterate through col array and find entries matching 'i'
+	//sum all rows for each col
+	//send summed row data for each col
+	for(uint8_t i = 0x01; i<=0x08; i++){	//do for each column
+		uint8_t row_sum=0;					//resets the sum for each column - saves 7 variables being declared!
+		for(uint8_t j=0; j<len; j++){		//go through each element of the snake
+			if(*(col+j) == i){				//if current element of array has same column as one being checked
+				row_sum += *(row+j);
+			}
+		}
+		SPI_data_send(i,row_sum);	
+	}
+	_delay_ms(500);
 }
 
  void move_snake(uint8_t *col,uint8_t *row,enum direction dir)
-{
+ {
 	cli();															//disable interrupts whilst dir is being used
-	switch (dir)
-	{
+	switch (dir){
 		case DOWN:
-			if(*row>0x01)
-			{
+			if(*row>0x01){
 				*row = *row >> 1;
-				sei();
+				//check if this position is already taken - todo
 				break;
 			}
 			//code to end game because boundary was hit
-			sei();
 			break;
 		
 		case UP:
-			if(*row<0x80)
-			{
+			if(*row<0x80){
 				*row = *row << 1;
-				sei();
 				break;
 			}
 			//code to end game because boundary was hit
-			sei();
 			break;
 		
 		case LEFT:
-			if(*col<0x08)									//do not move left if in leftmost column register
-			{
-				//SPI_data_send(*col,0x00);					//clear current row
+			if(*col<0x08){									//do not move left if in leftmost column register
 				(*col)++;									//move to column on left
-				sei();
 				break;				
 			}
 			//code to end game because boundary was hit
-			sei();
 			break;
 			
 		case RIGHT:
-			if(*col>0x01)
-			{
-				//SPI_data_send(*col,0x00); //clear current row
+			if(*col>0x01){
 				(*col)--;
-				sei();
 				break;				
 			}
 			//code to end game because boundary was hit
-			sei();
 			break;
 	}
+	sei();													//re-enable interrupts now we are finished
 }
 
 void store_moves(uint8_t* col,uint8_t* row ,uint8_t len)
 {
-	for (int i = (len-1); i>0; i--)
-	{
-		*(col+i) = *(col+i-1);
+	for (int i = (len-1); i>0; i--){		//reverse iterate through array
+		*(col+i) = *(col+i-1);				//shift values along by one
+	}
+	for (int i = (len-1); i>0; i--){
+		*(row+i) = *(row+i-1);
 	}
 }
-//Give it a tail!
+//Add food
+//Add lose conditions
+//Add win condition
