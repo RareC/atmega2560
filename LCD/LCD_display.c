f536336f86482788e9fe8911a003b69ce7825edd
chrislyford <35743417+chrislyford@users.noreply.github.com>
2018-03-14 23:01:28 +0000

    Adding right scrolling and fixed clear screen
    Todo: add left scroll + fix other functions to work with both left and right scroll; make into header/source files

--- LCD/LCD_display.c
+++ LCD/LCD_display.c
@@ -10,12 +10,9 @@
  */ 
 #define F_CPU 16000000
 
-#define  D7 0x08
-#define  D6 0x04
-#define  D5 0x02
-#define  D4 0x01
 #define  RS 0x02
 #define  E  0x01
+#define  SCROLL_SPEED 300					//delay in ms between writing/moving characters
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -28,7 +25,9 @@ void LCD_write_word(uint8_t*);
 void LCD_init();
 void LCD_move_curs(uint8_t, uint8_t);
 void LCD_clear_display();
-//void LCD_scroll_right();
+void LCD_clear_line(uint8_t);
+void LCD_clear_trail();
+void LCD_scroll_right(uint8_t*,uint8_t);
 //void LCD_scroll_left();
 void LCD_disp_num(int);
 void LCD_vintage_write(uint8_t*);
@@ -38,11 +37,10 @@ int main(void)
 	DDRC |= 0x0F;						//set bottom half of port C as output
 	DDRG |= 0x03;						//set bottom three bits of port G as output
 	LCD_init();
-	uint8_t lol [] = "This is a test";
-	LCD_move_curs(2,0);
-	LCD_vintage_write(lol);
+	uint8_t lol [] = "short";
 	LCD_move_curs(1,0);
-	LCD_disp_num(-32767);
+	LCD_vintage_write(lol);
+	LCD_scroll_right(lol,2);
     while (1) 
     {
 		
@@ -52,11 +50,11 @@ void LCD_send_data(uint8_t data){
 	PORTG |= E;
 	PORTC = data >> 4;			//send top nibble
 	PORTG &= ~E;
-	_delay_ms(1);
+	_delay_us(37);
 	PORTG |= E;
 	PORTC = data;				//send lower nibble
 	PORTG &= ~E;
-	_delay_ms(1);
+	_delay_us(37);
 }
 
 void LCD_write_cmd(uint8_t command){
@@ -87,31 +85,58 @@ void LCD_move_curs(uint8_t line, uint8_t offset){
 }
 
 void LCD_clear_display(){				//needs own function due to longer execution time
+	PORTG &= ~RS;						//sending command so set RS low
 	PORTG |= E;
 	PORTC = 0x00;						//send top nibble
 	PORTG &= ~E;
-	_delay_ms(5);
+	_delay_ms(2);
 	PORTG |= E;
 	PORTC = 0x01;						//send lower nibble
 	PORTG &= ~E;
-	_delay_ms(5);
+	_delay_ms(2);
 }
 
-void LCD_vintage_write(uint8_t *word){	//same as write word but with cursor blinking like old style terminal
+void LCD_clear_line(uint8_t line){		//clears a single line instead of the entire display
+	LCD_move_curs(line,0);
+	for (uint8_t i =0; i < 16; i++){
+		LCD_write_char(0x11);
+	}
+}
+
+void LCD_vintage_write(uint8_t *word){	//same as LCD_write_word but with cursor blinking like old style terminal
 	LCD_write_cmd(0x0F);				//turn on blinking cursor
 	uint8_t i = 0;
 	while(*(word+i)){
 		LCD_write_char(*(word+i));
 		i++;
-		_delay_ms(150);
+		_delay_ms(SCROLL_SPEED);
 	}
 	LCD_write_cmd(0x0C);				//disable blinking cursor
 }
 
 void LCD_disp_num(int val){
-	uint8_t buff [11];					//max value of 4 bytes has 10 digits plus 1 for sign
+	uint8_t buff [6];					//max value of 2 bytes has 5 digits plus 1 for sign
 	itoa(val,buff,10);
 	LCD_write_word(buff);
+}
+
+void LCD_scroll_right(uint8_t *word,uint8_t line){			
+	uint8_t start_pos = 16;
+	LCD_move_curs(line,start_pos-1);
+	while(start_pos){
+		//LCD_clear_line(line);
+		LCD_write_word(word);
+		LCD_clear_trail();
+		start_pos--;
+		LCD_move_curs(line,start_pos-1);
+		_delay_ms(SCROLL_SPEED);
+	}	
+}
+
+void LCD_clear_trail(){						//clear trailing characters with scrolling text
+	for(uint8_t i = 0; i < 16; i++){		//line 2 DRAM addresses will never be reached so okay
+		LCD_write_char(0x11);
+	}	
 }
 
 void LCD_init(){
