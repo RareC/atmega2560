98e86f6b616f1353311233429b8e6cd774cb3d30
chrislyford <chrislyford33@hotmail.com>
2018-09-05 17:40:59 +0100

    Added forwards and backwards control

--- Projects/Remote Controlled Car/PWM motor with ADC.c
+++ Projects/Remote Controlled Car/PWM motor with ADC.c
@@ -18,28 +18,43 @@ void change_speed();
 
 int main(void){
 	DDRB |= 0x80;
+	DDRG |= 0x20;
     init_pwm();
 	init_adc();
     while (1) {
 		change_speed();
-    }
+		_delay_ms(20);
+    } 
 }
 
 void init_pwm(){
-	TCCR0A |= (1<<COM0A1)|(1<<WGM01)|(1<<WGM00);		//Set PWM in non-inverting mode, and to re-set pin at top of counter
-	TCCR0B |= (1<<CS01) | (1<<CS00);					//Use f/64 pre-scaler for frequency - arbitrary
-	OCR0A = 0xFF;										//Duty Cycle of 25%
+	TCCR0A |= (1<<COM0A1)|(1<<WGM01)|(1<<WGM00);	//Set PWM in non-inverting mode, and to re-set pin at top of counter
+	TCCR0B |= (1<<CS01) | (1<<CS00);				//Use f/64 pre-scaler for frequency - arbitrary
 }
 
 void init_adc()
 {
-	ADCSRA |= (1<<ADEN);							//enable ADC
-	ADMUX |= (1<<REFS0)|(1<<ADLAR);													//use 2.56V ref & left adjust result, using ADC0
-	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);    //set ADC freq pre-scaler = F_CPU/128
+	ADCSRA |= (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);	//enable ADC, set ADC freq pre-scaler = F_CPU/128
+	ADMUX |= (1<<REFS0)|(1<<ADLAR);					//AVcc ref & left adjust result, using ADC0
 }
 
 void change_speed(){
 	ADCSRA |= (1<<ADSC);							//begin conversion
-	while (ADCSRA & (1<<ADSC)){}					//wait for conversion
-	OCR0A = ADCH;		
+	while (ADCSRA & (1<<ADSC));						//wait for conversion to finish
+	
+	if (ADCH > 0x8A){								//Forwards
+		TCCR0A &= ~(1<<COM0B1);						//disable PWM output on OCR0B if not already
+		TCCR0A |= (1<<COM0A1);						//enable PWM output on OCR0A
+		OCR0A = ADCH;								
+	}		
+	
+	else if (ADCH <= 0x8A && ADCH >= 0x7D){			//Standstill
+		TCCR0A &= ~((1<<COM0A1)|(1<<COM0B1));		//Disable both PWM outputs
+	}
+	
+	else {											//Backwards
+		TCCR0A &= ~(1<<COM0A1);						//Disable PWM output on OCR0A
+		TCCR0A |= (1<<COM0B1);						//enable PWM output on ORC0B
+		OCR0B = 0xFF - ADCH;						//ADCH is 8 bit value & low values mean higher reverse speed
+	}
 }
